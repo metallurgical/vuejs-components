3b13463a011793746b078ccfa9d01f486a832f48
Norlihazmey Ghazali <norlihazmey.ghazali@gmail.com>
2019-07-15 04:14:03 +0800

    Create v-input component

---
+++ bootstrap-4/V-input.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="form-group row" v-if="inputType === 'column'">
+        <label :for="name" class="col-sm-2 col-form-label">{{ label }}</label>
+        <div class="col-sm-10">
+            <input :type="inputType" class="form-control" :id="name" :value="inputValue" :placeholder="placeholder"
+                   v-on:input="$emit('input', $event.target.value)">
+        </div>
+    </div>
+
+    <div class="form-group" v-else>
+        <label :for="name">{{ label }}</label>
+        <input :type="inputType" class="form-control" :id="name" :value="inputValue" :placeholder="placeholder"
+               v-on:input="$emit('input', $event.target.value)">
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            inputType: {
+                type: String,
+                default: 'row',
+            },
+            type: {
+                type: String,
+                default: 'text'
+            },
+            label: {
+                type: String,
+                required: true
+            },
+            // Required if sending traditional FORM action
+            name: {
+                type: String,
+            },
+            // For input's default value
+            inputValue: {
+                type: String,
+                default: ''
+            },
+            placeholder: {
+                type: String,
+                default: ''
+            },
+            // For simulate v-model
+            value: {
+                type: String,
+            }
+        }
+    }
+</script>
